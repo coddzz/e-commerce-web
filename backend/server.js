68544d06c8bad1957baae958476e59645c1d34a0
Sarath <learn.cypher@gmail.com>
2025-09-06 23:22:22 +0530

    dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config()
+const PORT = process.env.PORT;
 
 app.get("/", (req, res)=>{
     res.send("hello World! e-commerce")
 })
 
-app.listen(5000, ()=>{
-    console.log(`Server Running On PORT: 5000`)
+app.listen(PORT, ()=>{
+    console.log(`Server Running On PORT: ${PORT}`)
 })
-
