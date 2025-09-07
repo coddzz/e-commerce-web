7215848fe69bc217555876924a6ae273dd43decd
Sarath <learn.cypher@gmail.com>
2025-09-08 01:43:45 +0530

    signup controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,23 @@
+import User from "../models/user.model.js"
 
-export const signup = (req, res)=>{
-    res.send("signup page!")
+export const signup = async (req, res)=>{
+    const { name, email, password } = req.body;
+    try{
+        const userExists = await User.findOne({email});
+        if(userExists){
+            res.status(400).json({message:"User Already"});
+        }
+
+        const user = await User.create({ name, email, password })
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+        });
+
+    } catch(error){
+        console.log(`Error in Signup Controller`,error.message)
+        res.status(500).json({message:error.message})
+    }
 }
