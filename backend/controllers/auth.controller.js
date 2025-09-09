b906bf6e9286b0b0cc851b563662c5cbaba34985
Sarath <learn.cypher@gmail.com>
2025-09-10 01:08:56 +0530

    Generate Tokens and Set Cookies syntax

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,38 @@
 import User from "../models/user.model.js"
+import jwt from "jsonwebtoken";
+
+const generateTokens = (userId) =>{
+    const accessToken = jwt.sign({userId}, process.env.ACCESS_TOKEN_SECRET,{
+        expiresIn: "15m",
+    });
+
+    const refreshToken = jwt.sign({userId}, process.env.REFRESH_TOKEN_SECRET,{
+        expiresIn: "7d",
+    });
+
+    return {accessToken, refreshToken }
+
+};
+
+const setCookies = (res, accessToken, refreshToken) =>{
+    res.cookie("accessToken", accessToken, {
+        maxAge: 15*60*1000, //15minutes.
+        httponly: true,     // XSS attack, cross site scripting attack.
+        sameSite: "scrict", // CSRF attacks, cross site forgery attack.
+        secure: process.env.NODE_ENV === "production",
+    })
+
+    res.cookie("refreshToken", refreshToken, {
+        maxAge: 7*24*60*60*1000, //7 days
+        httponly: true,
+        sameSite: "strict",
+        secure: process.env.NODE_ENV === "production",
+    })
+
+
+
+} 
+
 
 export const signup = async (req, res)=>{
     const { name, email, password } = req.body;
--- backend/server.js
+++ backend/server.js
@@ -18,5 +18,5 @@ app.get("/", (req, res)=>{
 
 app.listen(PORT, ()=>{
     console.log(`Server Running On PORT: ${PORT}`)
-    connectDB();
+    //connectDB();
 })
