156c7de8128773b226374f91b6c60646946c727a
Sarath <learn.cypher@gmail.com>
2025-09-17 18:04:02 +0530

    add/remove/update cartItems -- cart.controller.js

--- backend/controllers/cart.controller.js
+++ backend/controllers/cart.controller.js
@@ -17,6 +17,76 @@ export const getCartProducts = async (req, res) =>{
 	}
 };
 
-// addToCart
-// removeAllFromCart
-// updateQuantity
+export const addToCart = async (req, res) => {
+	try {
+
+        
+        // const productId and user
+        // construct exitingitem (add +1 or push product to cartItems)
+        // save the user and print cartItems
+
+        const { productId } = req.body;
+        const user = req.user;
+
+        const exitingItem = req.cartItems.find((item) => item.id === productId );
+        if(exitingItem){
+            exitingItem.quantity += 1;
+        } else{
+            user.cartItems.push(productId);
+        }
+
+        await user.save();
+        res.json(user.cartItems);
+
+	} catch (error) {
+		console.log("Error in addToCart controller", error.message);
+		res.status(500).json({ message: "Server error", error: error.message });
+	}
+};
+
+export const removeAllFromCart = async (req, res) => {
+	try {
+
+        const { productId } = req.body;
+        const user = req.user;
+
+        if(!productId){
+            user.cartItems = [];
+        } else{
+            user.cartItems = user.cartItems.filter((item)=> item.id !== productId);
+        }
+
+        await user.save();
+        res.json(user.cartItems);
+
+	} catch (error) {
+		res.status(500).json({ message: "Internal Server error", error: error.message });
+	}
+};
+
+export const updateQuantity = async (req, res) => {
+	try {
+		const { id: productId } = req.params;
+		const { quantity } = req.body;
+		const user = req.user;
+		const existingItem = user.cartItems.find((item) => item.id === productId);
+
+		if (existingItem) {
+			if (quantity === 0) {
+				user.cartItems = user.cartItems.filter((item) => item.id !== productId);
+				await user.save();
+				return res.json(user.cartItems);
+			}
+
+			existingItem.quantity = quantity;
+			await user.save();
+			res.json(user.cartItems);
+		} else {
+			res.status(404).json({ message: "Product not found" });
+		}
+
+	} catch (error) {
+		console.log("Error in updateQuantity controller", error.message);
+		res.status(500).json({ message: "Internal Server error", error: error.message });
+	}
+};
